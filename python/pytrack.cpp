467240d01906b3e6282ecfcf7ba7840564e9487a
Carsten Haubold <carstenhaubold@googlemail.com>
2014-02-19 17:28:43 +0100

    Make block size and overlap of decomposition available in python.

--- python/pytrack.cpp
+++ python/pytrack.cpp
@@ -1,6 +1,6 @@
 #define PY_ARRAY_UNIQUE_SYMBOL pgmlink_pyarray
 #define NO_IMPORT_ARRAY
-#define BOOST_PYTHON_MAX_ARITY 22
+#define BOOST_PYTHON_MAX_ARITY 25
 
 #include <vector>
 
@@ -77,15 +77,20 @@ void export_track() {
 	;
 
     class_<ConsTrackingDD>("ConsTrackingDD",
-                         init<int,double,double,string,bool,double,double,double,bool,double,double,bool,double,double, bool, int, double, double, FieldOfView, bool>(
+                         init<int,double,double,string,bool,
+                         double,double,double,bool,double,double,
+                         bool,double,double, bool, int, double,
+                         double, FieldOfView, bool, int, int>(
                         args("max_number_objects", "max_neighbor_distance", "division_threshold",
                             "detection_rf_filename", "size_dependent_detection_prob", "forbidden_cost",
                             "ep_gap", "avg_obj_size",
                             "with_tracklets",
                             "division_weight", "transition_weight",
                             "with_divisions",
-                             "disappearance_cost", "appearance_cost", "with_merger_resolution", "number_of_dimensions",
-                             "transition_parameter", "border_width", "fov", "with_constraints")))
+                             "disappearance_cost", "appearance_cost", "with_merger_resolution",
+                             "number_of_dimensions",
+                             "transition_parameter", "border_width", "fov", "with_constraints",
+                             "timesteps_per_block", "num_overlapping_timesteps")))
       .def("__call__", &ConsTrackingDD::operator())
       .def("detections", &ConsTrackingDD::detections)
     ;
